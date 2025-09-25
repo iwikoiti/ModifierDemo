a6a6fbf6f1336164f0b552991247537089200b7c
yui <yuineor@gmail.com>
2025-09-25 18:09:47 +0700

    add fun CustomImage with modifier

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,4 +55,11 @@ fun DefaultPreview() {
     }
 }
 
-
+@Composable
+fun CustomImage(image: Int, modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(image),
+        contentDescription = null,
+        modifier
+    )
+}
